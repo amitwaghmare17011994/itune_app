a025018e27e436b78d6b61f751d2d29e402822ad
Amit <amitwaghamare17@gmail.com>
2020-06-19 06:12:11 -1200

    minor changes

--- src/LayoutComponents/BodyComponents/ErrorMessage.js
+++ src/LayoutComponents/BodyComponents/ErrorMessage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Empty } from "antd";
 
-const ErrorMessage = ({msg}) => <Empty description={msg}/>;
+const ErrorMessage = ({ msg }) => (
+  <Empty description={msg} className="empty_message" />
+);
 
 export default ErrorMessage;
