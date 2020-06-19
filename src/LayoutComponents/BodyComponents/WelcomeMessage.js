66a300abf765ab2a608e01f23b9e5211f4ad95e7
Amit <amitwaghamare17@gmail.com>
2020-06-18 18:45:41 -1200

    welcome message added

--- src/LayoutComponents/BodyComponents/WelcomeMessage.js
+++ src/LayoutComponents/BodyComponents/WelcomeMessage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const WelcomeMessage = () => {
   return (
-      <h2 style={{color:'white'}}>Welcome to ItuneApp</h2>
+      <h2 style={{color:'white',fontSize:35}}>Welcome to ItuneApp</h2>
   );
 };
 export default WelcomeMessage;
