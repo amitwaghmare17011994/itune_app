14e1fd802c8d983debcccffc9a38696291aa7d6e
Amit <amitwaghamare17@gmail.com>
2020-06-18 19:59:47 -1200

    isEmptyString check added

--- src/Sagas/saga.js
+++ src/Sagas/saga.js
@@ -2,21 +2,25 @@ import { call, put, takeEvery, takeLatest } from "redux-saga/effects";
 import SagaActionTypes from "./sagaActionTypes";
 import { searchSongsApi } from "../Services/songs";
 import Types from "../Reducers/actionTypes";
-import { filterResultBySongs } from "../Helpers/helpers";
+import { filterResultBySongs, isEmptyString } from "../Helpers/helpers";
 
 function* fetchSongsWorker(action) {
   try {
-    yield put({ type: Types.SET_SHOW_LOADER, showLoader: true });
-    const response = yield call(searchSongsApi, action.searchedTerm);
-    const filteredSongs = filterResultBySongs(response);
-    yield put({
-      type: Types.UPDATE_SONGS,
-      songs: filterResultBySongs(filteredSongs),
-    });
-    if (filteredSongs && filteredSongs.length) { // For Empty Array Do Not Remove Searched Term From Searched Field 
-      yield put({ type: Types.UPDATE_SEARCHED_TERM, searchedTerm: "" });
+    const { searchedTerm } = action;
+    if (!isEmptyString(searchedTerm)) {
+      yield put({ type: Types.SET_SHOW_LOADER, showLoader: true });
+      const response = yield call(searchSongsApi, searchedTerm);
+      const filteredSongs = filterResultBySongs(response);
+      yield put({
+        type: Types.UPDATE_SONGS,
+        songs: filterResultBySongs(filteredSongs),
+      });
+      if (filteredSongs && filteredSongs.length) {
+        // For Empty Array Do Not Remove Searched Term From Searched Field
+        yield put({ type: Types.UPDATE_SEARCHED_TERM, searchedTerm: "" });
+      }
+      yield put({ type: Types.SET_SHOW_LOADER, showLoader: false });
     }
-    yield put({ type: Types.SET_SHOW_LOADER, showLoader: false });
   } catch (e) {
     yield put({ type: Types.SET_ERROR, errorMessage: e.message });
     yield put({ type: Types.SET_SHOW_LOADER, showLoader: false });
