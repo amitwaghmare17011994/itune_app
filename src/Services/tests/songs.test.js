d0707f674cfc09f606104cc5cefe8b0b2b1f4829
Amit <amitwaghamare17@gmail.com>
2020-06-20 15:05:14 -1200

    minor changees

--- src/Services/tests/songs.test.js
+++
@@ -1,8 +0,0 @@
-import {searchSongsApi} from '../songs'
-describe('Services Tests', () => {
-    it('it should return promise',()=>{
-        const searchSongsPromise = searchSongsApi();
-        console.log(searchSongsPromise);
-    })
-
-})
